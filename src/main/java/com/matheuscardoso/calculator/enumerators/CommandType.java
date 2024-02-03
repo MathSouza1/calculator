cd6ade3cfcd6ababea8e4cfb67c9c58f9baa5241
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-03 02:14:16 -0300

    Removing unnecessary semicolon at CommandType.java

--- src/main/java/com/matheuscardoso/calculator/enumerators/CommandType.java
+++ src/main/java/com/matheuscardoso/calculator/enumerators/CommandType.java
@@ -1,5 +1,5 @@
 package com.matheuscardoso.calculator.enumerators;
 
 public enum CommandType {
-    RESET, SIGNAL, NUMBER, DIVIDED, TIMES, MINUS, PLUS, EQUALS, COMMA;
+    RESET, SIGNAL, NUMBER, DIVIDED, TIMES, MINUS, PLUS, EQUALS, COMMA
 }
