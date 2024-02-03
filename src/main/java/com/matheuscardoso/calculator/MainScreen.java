afa7ab05e4b19920470d697fd15772aa06e0615a
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-02 22:45:35 -0300

    Adding layoutOrganizer method, to align the display at north and the keyboard at the center

--- src/main/java/com/matheuscardoso/calculator/MainScreen.java
+++ src/main/java/com/matheuscardoso/calculator/MainScreen.java
@@ -1,17 +1,35 @@
 package com.matheuscardoso.calculator;
 
+import com.matheuscardoso.calculator.components.Display;
+import com.matheuscardoso.calculator.components.Keyboard;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class MainScreen extends JFrame {
 
     private static final int CALCULATOR_WIDTH = 232;
     private static final int CALCULATOR_HEIGHT = 322;
+    private static final int DIMENSION_WIDTH = 233;
+    private static final int DIMENSION_HEIGHT = 60;
 
     public MainScreen() {
+        layoutOrganizer();
+
         setSize(CALCULATOR_WIDTH, CALCULATOR_HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void layoutOrganizer() {
+        setLayout(new BorderLayout());
+        Display display = new Display();
+        display.setPreferredSize(new Dimension(DIMENSION_WIDTH, DIMENSION_HEIGHT));
+        add(display, BorderLayout.NORTH);
+
+        Keyboard keyboard = new Keyboard();
+        add(keyboard, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
