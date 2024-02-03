63f153dd650db76d9b63f7f54f7c40234a247535
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-03 00:35:30 -0300

    Adding addButton method at Keyboard.java to get a better layout

--- src/main/java/com/matheuscardoso/calculator/components/Keyboard.java
+++ src/main/java/com/matheuscardoso/calculator/components/Keyboard.java
@@ -2,8 +2,10 @@ package com.matheuscardoso.calculator.components;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Keyboard extends JPanel {
+public class Keyboard extends JPanel  {
 
     private static final int NUMBER_ROWS_LAYOUT = 5;
     private static final int NUMBER_COLUMNS_LAYOUT = 4;
@@ -12,30 +14,46 @@ public class Keyboard extends JPanel {
     private static final Color ORANGE = new Color(242, 163, 60);
 
     public Keyboard() {
-        setLayout(new GridLayout(NUMBER_ROWS_LAYOUT, NUMBER_COLUMNS_LAYOUT));
-        add(new Button("AC", DARK_GRAY));
-        add(new Button("+/-", DARK_GRAY));
-        add(new Button("%", DARK_GRAY));
-        add(new Button("/", ORANGE));
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+        setLayout(layout);
 
-        add(new Button("7", LIGHT_GRAY));
-        add(new Button("8", LIGHT_GRAY));
-        add(new Button("9", LIGHT_GRAY));
-        add(new Button("*", ORANGE));
+        constraints.weightx = 1;
+        constraints.weighty = 1;
+        constraints.fill = GridBagConstraints.BOTH;
 
-        add(new Button("7", LIGHT_GRAY));
-        add(new Button("8", LIGHT_GRAY));
-        add(new Button("9", LIGHT_GRAY));
-        add(new Button("*", ORANGE));
+        constraints.gridwidth = 2;
+        addButton("AC", DARK_GRAY, constraints, 0, 0);
+        constraints.gridwidth = 1;
+        addButton("±", DARK_GRAY, constraints, 2, 0);
+        addButton("/", ORANGE, constraints, 3, 0);
 
-        add(new Button("7", LIGHT_GRAY));
-        add(new Button("8", LIGHT_GRAY));
-        add(new Button("9", LIGHT_GRAY));
-        add(new Button("*", ORANGE));
+        addButton("7", LIGHT_GRAY, constraints, 0, 1);
+        addButton("8", LIGHT_GRAY, constraints, 1, 1);
+        addButton("9", LIGHT_GRAY, constraints, 2, 1);
+        addButton("*", ORANGE, constraints, 3, 1);
 
-        add(new Button("7", LIGHT_GRAY));
-        add(new Button("8", LIGHT_GRAY));
-        add(new Button("9", LIGHT_GRAY));
-        add(new Button("*", ORANGE));
+        addButton("4", LIGHT_GRAY, constraints, 0, 2);
+        addButton("5", LIGHT_GRAY, constraints, 1, 2);
+        addButton("6", LIGHT_GRAY, constraints, 2, 2);
+        addButton("-", ORANGE, constraints, 3, 2);
+
+        addButton("1", LIGHT_GRAY, constraints, 0, 3);
+        addButton("2", LIGHT_GRAY, constraints, 1, 3);
+        addButton("3", LIGHT_GRAY, constraints, 2, 3);
+        addButton("+", ORANGE, constraints, 3, 3);
+
+        constraints.gridwidth = 2;
+        addButton("0", LIGHT_GRAY, constraints, 0, 4);
+        constraints.gridwidth = 1;
+        addButton(",", LIGHT_GRAY, constraints, 2, 4);
+        addButton("=", ORANGE, constraints, 3, 4);
+    }
+
+    private void addButton(String text, Color color, GridBagConstraints constraints, int x, int y) {
+        constraints.gridx = x;
+        constraints.gridy = y;
+        Button button = new Button(text, color);
+        add(button, constraints);
     }
 }
